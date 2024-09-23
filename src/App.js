71e03ae7593de5e5802667b7ce1979ee296b9a7f
pedrovitorsilva <pedrovitoros11@gmail.com>
2024-09-22 21:27:04 -0300

    Alterações do logo e correção das rotas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './autheContext';
+
 import Login from './Login';
 import Signup from './Signup';
 import Home from './Home';
@@ -9,17 +10,24 @@ import Rastreio from './Rastreio';
 
 function App() {
     return (
-        <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/pedido" element={<Pedido />} />
-                    <Route path="/rastreio" element={<Rastreio />} />
-                </Routes>
-            </Router>
-        </AuthProvider>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            {/* Cria a rota padrão*/}
+            <Route exact path="/" element={<Login />} />
+
+            {/* Demais rotas*/}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/pedido" element={<Pedido />} />
+            <Route path="/rastreio" element={<Rastreio />} />
+
+            {/* Rota padrão, em caso de rota inexistente */}
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
     );
 }
 
